707eb4ed280318bd82ee0a39f41d66c8b84c8210
nicolas3105 <lefebvrelefebvre380@yahoo.fr>
2025-03-31 09:03:18 +0200

    Initial commit

---
+++ src/main/java/com/unilasalle/tpwebavance_back/TpWebAvanceBackApplication.java
@@ -0,0 +1,13 @@
+package com.unilasalle.tpwebavance_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpWebAvanceBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpWebAvanceBackApplication.class, args);
+    }
+
+}
